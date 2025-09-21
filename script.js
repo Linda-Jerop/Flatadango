ccc89551f1be66e80615e600e8745258dfce6308
Linda <jeropkoimur@gmail.com>
2025-09-21 20:58:46 +0300

    third

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+// Fetches ALL movies
+fetch("http://localhost:3000/films")
+  .then(res => res.json())
+  .then(movies => {
+    // shows the first movie by default
+    showMovieDetails(movies[0]);
 
 
