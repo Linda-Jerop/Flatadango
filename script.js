d17f9a49640b7897461269ebf087af4d026cda5e
Linda <jeropkoimur@gmail.com>
2025-09-21 21:01:05 +0300

    fourth

--- script.js
+++ script.js
@@ -5,4 +5,23 @@ fetch("http://localhost:3000/films")
     // shows the first movie by default
     showMovieDetails(movies[0]);
 
+    // also lists all movies on the left side
+    const filmsList = document.getElementById("films");
+    filmsList.innerHTML = ""; // clear placeholder <li>
+
+    movies.forEach(movie => {
+      const li = document.createElement("li");
+      li.textContent = movie.title;
+      li.classList.add("film", "item");
+
+      // event: click on a movie ---> shows its details
+      li.addEventListener("click", () => {
+        showMovieDetails(movie);
+      });
+
+      filmsList.appendChild(li);
+    });
+  });
+
+
 
